476d4974166360e7e5441c6150fedb64e4c1b682
farhan <mofarhannn@gmail.com>
2026-04-06 01:39:13 +0700

    feat: add user, comments, and likes relationships to Post model

--- apiminisocmed/app/Models/Post.php
+++ apiminisocmed/app/Models/Post.php
@@ -16,4 +16,16 @@ class Post extends Model
         'content',
         'image_url',
     ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes() {
+        return $this->hasMany(Like::class);
+    }
 }
